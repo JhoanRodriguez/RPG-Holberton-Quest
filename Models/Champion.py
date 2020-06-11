9e35753fb00da44e10ac5a4a2cbb48f1df64d354
JhoanRodriguez <jhoansebastian@msn.com>
2020-06-10 23:01:53 -0500

    Storing data in dictionary

--- Models/Champion.py
+++ Models/Champion.py
@@ -1,3 +1,5 @@
+import json
+
 
 class Champion:
 
@@ -85,3 +87,59 @@ class Champion:
     def speed(self, value):
         if value < 101:
             self.__speed = value
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        This functions returns the json string representation
+        of list_dictionaries
+        """
+        if list_dictionaries is None:
+            list_dictionaries = []
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """
+        This function writes the json
+        string representation of list_objs
+        to a file
+        """
+        filename = cls.__name__ + ".json"
+        newlist = []
+        if list_objs is None:
+            cls.to_json_string(list_objs)
+        else:
+            for item in list_objs:
+                newlist.append(cls.to_dictionary(item))
+        with open(filename, "w") as Myfile:
+            Myfile.write(cls.to_json_string(newlist))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """
+        This function returns the list
+        of the json string represenation
+        """
+        if json_string is None or len(json_string) == 0:
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def load_from_file(cls):
+        """
+        This function returns a list
+        of instances
+        """
+        filename = cls.__name__ + ".json"
+        newlist = []
+        if cls is None:
+            return newlist
+        try:
+            with open(filename, "r") as Myfile:
+                newlist = cls.from_json_string(Myfile.read())
+            for item in range(len(newlist)):
+                newlist[item] = cls.create(**newlist[item])
+        except Exception:
+            pass
+        return newlist
--- main.py
+++ main.py
@@ -4,9 +4,12 @@ pygame.init()
 size = 800, 600
 screen = pygame.display.set_mode(size)
 run = True
+wait = [True, True, True]
 pygame.display.set_caption("RPG Holberton Quest")
 icon = pygame.image.load("../Images/Holberton.png")
 pygame.display.set_icon(icon)
+pygame.mouse.set_pos([400, 300])
+data = {}
 
 # Position
 gender1x = 150
@@ -21,7 +24,12 @@ gender2 = pygame.image.load("../Images/Female.png")
 gender2 = pygame.transform.scale(gender2, (180, 280))
 
 
-def gender():
+def fgender():
+    screen.blit(gender1, (gender1x, gender1y))
+    screen.blit(gender2, (gender2x, gender2y))
+
+
+def frace():
     screen.blit(gender1, (gender1x, gender1y))
     screen.blit(gender2, (gender2x, gender2y))
 
@@ -30,9 +38,24 @@ def gender():
 while run:
     pygame.display.update()
     screen.fill((50, 50, 50))
+    mouse = pygame.mouse.get_pos()
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+        elif event.type == pygame.MOUSEBUTTONDOWN and wait[0]:
+            if (150 < mouse[0] < 300) and (150 < mouse[1] < 450):
+                data.update({"gender": "Male"})
+                print(data)
+                wait[0] = False
+            elif (450 < mouse[0] < 630) and (150 < mouse[1] < 430):
+                gender = "Female"
+                data.update({"gender": "Female"})
+                wait[0] = False
+                print(data)
+    if wait[0]:
+        fgender()
+    if wait[0] is False and wait[1]:
+        frace()
 
-    gender()
 pygame.quit()
