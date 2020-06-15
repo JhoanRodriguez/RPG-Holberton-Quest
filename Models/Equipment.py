66533927def204faaf7ae9b298dac8f01d761ef5
JhoanRodriguez <jhoansebastian@msn.com>
2020-06-15 01:16:32 -0500

    Updating the Armor and the fight variables

---
+++ Models/Equipment.py
@@ -0,0 +1,30 @@
+class Equipment:
+
+    def __init__(self, name="Weapon/Armor"):
+        self.name = name
+        self.damage = 5
+        self.defense = 0
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, value):
+        self.__name = value
+
+    @property
+    def damage(self):
+        return self.__damage
+
+    @damage.setter
+    def damage(self, value):
+        self.__damage = value
+
+    @property
+    def defense(self):
+        return self.__defense
+
+    @defense.setter
+    def defense(self, value):
+        self.__defense = value
