c453242ffb75eebe17544c02eedcafae12ea6d69
ElkinAMG <elkinandresmejiagueto@gmail.com>
2020-06-17 21:40:11 -0500

    Fight Updated

--- Scenes/attack_anim.py
+++ Scenes/attack_anim.py
@@ -1,99 +1,102 @@
 from time import sleep
-coor = [220, 360]
+coor = [0, 0]
 angle = 90
 clock = pygame.time.Clock()
-
-while coor[0] < 350 or coor[1] > 250:
-    clock.tick(45)
-    pygame.display.update()
-    pygame.display.flip()
-
-    screen.blit(back, (0, 0))
-    screen.blit(bat_bg, (0, 400))
-    screen.blit(avatar, (5, 420))
-    screen.blit(player_health, (7, 530))
-
-    screen.blit(enemy_, (280, 130))
-
-    screen.blit(attack, (150, 430))
-    screen.blit(phy, (200, 455))
-
-    screen.blit(magic, (150, 500))
-    screen.blit(chol, (200, 528))
-
-    screen.blit(surrender, (500, 460))
-    screen.blit(stats, (500, 490))
-
-    pygame.draw.line(screen, (255, 255, 255), (0, 400), (800, 400), 8)
-
-    weapon = pygame.transform.rotate(weapon, angle)
-
-    screen.blit(weapon, (int(coor[0]), int(coor[1])))
-    coor[0], coor[1] = coor[0] + 7, coor[1] - 7
 
 a_player = utilities.r_text("You've caused {:.2f} damage".format(damages[0]), (218, 165, 32),
                             "./Assets/Fonts/bitwise.ttf", 30)
 a_monster = utilities.r_text("-{:.2f} health".format(damages[1]), (220, 20, 60),
                              "./Assets/Fonts/bitwise.ttf", 30)
 
-# Monster Animation
-coor[0], coor[1] = 280, 130
-
-while coor[0] < 340:
-    clock.tick(45)
-
-    pygame.display.update()
-    pygame.display.flip()
-
-    screen.blit(back, (0, 0))
-    screen.blit(bat_bg, (0, 400))
-    screen.blit(avatar, (5, 420))
-    screen.blit(player_health, (7, 530))
-
-    screen.blit(enemy_, (coor[0], coor[1]))
-
-    screen.blit(attack, (150, 430))
-    screen.blit(phy, (200, 455))
-
-    screen.blit(magic, (150, 500))
-    screen.blit(chol, (200, 528))
-
-    screen.blit(surrender, (500, 460))
-    screen.blit(stats, (500, 490))
-
-    pygame.draw.line(screen, (255, 255, 255), (0, 400), (800, 400), 8)
-
-    coor[0] += 8
-
-while coor[0] >= 240:
-    clock.tick(45)
-
-    pygame.display.update()
-    pygame.display.flip()
-
-    screen.blit(back, (0, 0))
-    screen.blit(bat_bg, (0, 400))
-    screen.blit(avatar, (5, 420))
-    screen.blit(player_health, (7, 530))
-
-    screen.blit(enemy_, (coor[0], coor[1]))
-
-    screen.blit(attack, (150, 430))
-    screen.blit(phy, (200, 455))
-
-    screen.blit(magic, (150, 500))
-    screen.blit(chol, (200, 528))
-
-    screen.blit(surrender, (500, 460))
-    screen.blit(stats, (500, 490))
-
-    pygame.draw.line(screen, (255, 255, 255), (0, 400), (800, 400), 8)
-
-    coor[0] -= 8
-
-
 if damages[2] == "player":
+    # Player attack
+
+    coor = [220, 360]
+    while coor[0] < 350 or coor[1] > 250:
+        clock.tick(45)
+        pygame.display.update()
+        pygame.display.flip()
+
+        screen.blit(back, (0, 0))
+        screen.blit(bat_bg, (0, 400))
+        screen.blit(avatar, (5, 420))
+        screen.blit(player_health, (7, 530))
+
+        screen.blit(enemy_, (280, 130))
+
+        screen.blit(attack, (150, 430))
+        screen.blit(phy, (200, 455))
+
+        screen.blit(magic, (150, 500))
+        screen.blit(chol, (200, 528))
+
+        screen.blit(surrender, (500, 460))
+        screen.blit(stats, (500, 490))
+
+        pygame.draw.line(screen, (255, 255, 255), (0, 400), (800, 400), 8)
+
+        weapon = pygame.transform.rotate(weapon, angle)
+
+        screen.blit(weapon, (int(coor[0]), int(coor[1])))
+        coor[0], coor[1] = coor[0] + 7, coor[1] - 7
+
+    # Monster Hitted
+    coor[0], coor[1] = 280, 130
+
+    while coor[0] < 340:
+        clock.tick(45)
+
+        pygame.display.update()
+        pygame.display.flip()
+
+        screen.blit(back, (0, 0))
+        screen.blit(bat_bg, (0, 400))
+        screen.blit(avatar, (5, 420))
+        screen.blit(player_health, (7, 530))
+
+        screen.blit(enemy_, (coor[0], coor[1]))
+
+        screen.blit(attack, (150, 430))
+        screen.blit(phy, (200, 455))
+
+        screen.blit(magic, (150, 500))
+        screen.blit(chol, (200, 528))
+
+        screen.blit(surrender, (500, 460))
+        screen.blit(stats, (500, 490))
+
+        pygame.draw.line(screen, (255, 255, 255), (0, 400), (800, 400), 8)
+
+        coor[0] += 8
+    while coor[0] >= 240:
+        clock.tick(45)
+
+        pygame.display.update()
+        pygame.display.flip()
+
+        screen.blit(back, (0, 0))
+        screen.blit(bat_bg, (0, 400))
+        screen.blit(avatar, (5, 420))
+        screen.blit(player_health, (7, 530))
+
+        screen.blit(enemy_, (coor[0], coor[1]))
+
+        screen.blit(attack, (150, 430))
+        screen.blit(phy, (200, 455))
+
+        screen.blit(magic, (150, 500))
+        screen.blit(chol, (200, 528))
+
+        screen.blit(surrender, (500, 460))
+        screen.blit(stats, (500, 490))
+
+        pygame.draw.line(screen, (255, 255, 255), (0, 400), (800, 400), 8)
+
+        coor[0] -= 8
+
     coor = [280, 130]
+
+    # Message dmg Player
 
     while coor[1] > 100:
         clock.tick(45)
@@ -122,9 +125,153 @@ if damages[2] == "player":
 
         coor[1] -= 1
 
-    sleep(0.3)
+    if monster.health <= 0:
+            monster_die = True
+
+    if not monster_die:
+        # Monster attack
+
+        coor[0], coor[1] = 280, 130
+
+        while coor[1] < 230:
+            clock.tick(45)
+
+            pygame.display.update()
+            pygame.display.flip()
+
+            screen.blit(back, (0, 0))
+            screen.blit(bat_bg, (0, 400))
+            screen.blit(avatar, (5, 420))
+            screen.blit(player_health, (7, 530))
+
+
+            screen.blit(attack, (150, 430))
+            screen.blit(phy, (200, 455))
+
+            screen.blit(magic, (150, 500))
+            screen.blit(chol, (200, 528))
+
+            screen.blit(surrender, (500, 460))
+            screen.blit(stats, (500, 490))
+
+            pygame.draw.line(screen, (255, 255, 255), (0, 400), (800, 400), 8)
+
+            screen.blit(enemy_, (coor[0], coor[1]))
+
+            coor[1] += 8   
+
+        # Message dmg Monster
+
+        coor[0], coor[1] = 280, 420
+        while coor[1] < 440:
+            clock.tick(45)
+            pygame.display.update()
+            pygame.display.flip()
+
+            screen.blit(back, (0, 0))
+            screen.blit(bat_bg, (0, 400))
+            screen.blit(avatar, (5, 420))
+            screen.blit(player_health, (7, 530))
+
+            screen.blit(enemy_, (280, 130))
+
+            screen.blit(attack, (150, 430))
+            screen.blit(phy, (200, 455))
+
+            screen.blit(magic, (150, 500))
+            screen.blit(chol, (200, 528))
+
+            screen.blit(surrender, (500, 460))
+            screen.blit(stats, (500, 490))
+
+            pygame.draw.line(screen, (255, 255, 255), (0, 400), (800, 400), 8)
+
+            if monster.health > 0:
+                screen.blit(a_monster, (coor[0], coor[1]))
+
+            coor[1] += 1
+
+        #  Life Left
+
+        if damages[0] > 0:
+            damaged = round(life - damages[1], 2)
+        else:
+            damaged = (life - damages[1])
+
+        if damaged <= 0:
+            damaged = 0.00
+
+        while life >= damaged:
+            pygame.display.update()
+            pygame.display.flip()
+
+            screen.blit(back, (0, 0))
+            screen.blit(bat_bg, (0, 400))
+            screen.blit(avatar, (5, 420))
+            screen.blit(player_health, (7, 530))
+
+            screen.blit(enemy_, (280, 130))
+
+            screen.blit(attack, (150, 430))
+            screen.blit(phy, (200, 455))
+
+            screen.blit(magic, (150, 500))
+            screen.blit(chol, (200, 528))
+
+            screen.blit(surrender, (500, 460))
+            screen.blit(stats, (500, 490))
+
+            pygame.draw.line(screen, (255, 255, 255), (0, 400), (800, 400), 8)
+
+            life = life - 0.05
+
+            player_health = utilities.r_text("{:.2f}".format(life), (220, 20, 60),
+                                            "./Assets/Fonts/bitwise.ttf", 30)
+
+        # END FIGHT
+
+        if life <= 0:
+            life = 0
+            player_health = utilities.r_text("{:.2f}".format(0), (220, 20, 60),
+                                "./Assets/Fonts/bitwise.ttf", 30)
+            player_die = True
+
+
+else:
+    # Monster attack
+
+    coor[0], coor[1] = 280, 130
+
+    while coor[1] < 230:
+        clock.tick(45)
+
+        pygame.display.update()
+        pygame.display.flip()
+
+        screen.blit(back, (0, 0))
+        screen.blit(bat_bg, (0, 400))
+        screen.blit(avatar, (5, 420))
+        screen.blit(player_health, (7, 530))
+
+        screen.blit(attack, (150, 430))
+        screen.blit(phy, (200, 455))
+
+        screen.blit(magic, (150, 500))
+        screen.blit(chol, (200, 528))
+
+        screen.blit(surrender, (500, 460))
+        screen.blit(stats, (500, 490))
+
+        pygame.draw.line(screen, (255, 255, 255), (0, 400), (800, 400), 8)
+
+        screen.blit(enemy_, (coor[0], coor[1]))
+
+        coor[1] += 8
+
+    # Monster dmg
 
     coor[0], coor[1] = 280, 420
+
     while coor[1] < 440:
         clock.tick(45)
         pygame.display.update()
@@ -151,11 +298,18 @@ if damages[2] == "player":
         screen.blit(a_monster, (coor[0], coor[1]))
 
         coor[1] += 1
-else:
-    coor[0], coor[1] = 280, 420
 
-    while coor[1] < 440:
-        clock.tick(45)
+    #  Life Left
+
+    if damages[0] > 0:
+        damaged = round(life - damages[1], 2)
+    else:
+        damaged = (life - damages[1])
+
+    if damaged <= 0:
+        damaged = 0.00
+
+    while life >= damaged:
         pygame.display.update()
         pygame.display.flip()
 
@@ -177,87 +331,140 @@ else:
 
         pygame.draw.line(screen, (255, 255, 255), (0, 400), (800, 400), 8)
 
-        screen.blit(a_monster, (coor[0], coor[1]))
+        life = life - 0.05
 
-        coor[1] += 1
+        player_health = utilities.r_text("{:.2f}".format(life), (220, 20, 60),
+                                        "./Assets/Fonts/bitwise.ttf", 30)
 
-    coor = [280, 130]
+    # END FIGHT
 
-    while coor[1] > 100:
-        clock.tick(45)
-        pygame.display.update()
-        pygame.display.flip()
+    if life <= 0:
+        life = 0
+        player_health = utilities.r_text("{:.2f}".format(0), (220, 20, 60),
+                                "./Assets/Fonts/bitwise.ttf", 30)
+        player_die = True
+    elif monster.health <= 0:
+        monster_die = True
 
-        screen.blit(back, (0, 0))
-        screen.blit(bat_bg, (0, 400))
-        screen.blit(avatar, (5, 420))
-        screen.blit(player_health, (7, 530))
+    player_health = utilities.r_text("{:.2f}".format(life), (225, 255, 255),
+                                "./Assets/Fonts/bitwise.ttf", 30)
+        
 
-        screen.blit(enemy_, (280, 130))
+    # Player attack
+    if not player_die:
+        coor = [220, 360]
+        while coor[0] < 350 or coor[1] > 250:
+            clock.tick(45)
+            pygame.display.update()
+            pygame.display.flip()
 
-        screen.blit(attack, (150, 430))
-        screen.blit(phy, (200, 455))
+            screen.blit(back, (0, 0))
+            screen.blit(bat_bg, (0, 400))
+            screen.blit(avatar, (5, 420))
+            screen.blit(player_health, (7, 530))
 
-        screen.blit(magic, (150, 500))
-        screen.blit(chol, (200, 528))
+            screen.blit(enemy_, (280, 130))
 
-        screen.blit(surrender, (500, 460))
-        screen.blit(stats, (500, 490))
+            screen.blit(attack, (150, 430))
+            screen.blit(phy, (200, 455))
 
-        pygame.draw.line(screen, (255, 255, 255), (0, 400), (800, 400), 8)
+            screen.blit(magic, (150, 500))
+            screen.blit(chol, (200, 528))
 
-        screen.blit(a_player, (coor[0], coor[1]))
+            screen.blit(surrender, (500, 460))
+            screen.blit(stats, (500, 490))
 
-        coor[1] -= 1
+            pygame.draw.line(screen, (255, 255, 255), (0, 400), (800, 400), 8)
 
-sleep(0.3)
+            weapon = pygame.transform.rotate(weapon, angle)
 
-#  Life Left
+            screen.blit(weapon, (int(coor[0]), int(coor[1])))
+            coor[0], coor[1] = coor[0] + 7, coor[1] - 7
 
-if damages[0] > 0:
-    damaged = round(life - damages[1], 2)
-else:
-    damaged = (life - damages[1])
+        # Monster Hitted
+        coor[0], coor[1] = 280, 130
 
-if damaged <= 0:
-    damaged = 0.00
+        while coor[0] < 340:
+            clock.tick(45)
 
-while life >= damaged:
-    pygame.display.update()
-    pygame.display.flip()
+            pygame.display.update()
+            pygame.display.flip()
 
-    screen.blit(back, (0, 0))
-    screen.blit(bat_bg, (0, 400))
-    screen.blit(avatar, (5, 420))
-    screen.blit(player_health, (7, 530))
+            screen.blit(back, (0, 0))
+            screen.blit(bat_bg, (0, 400))
+            screen.blit(avatar, (5, 420))
+            screen.blit(player_health, (7, 530))
 
-    screen.blit(enemy_, (280, 130))
+            screen.blit(enemy_, (coor[0], coor[1]))
 
-    screen.blit(attack, (150, 430))
-    screen.blit(phy, (200, 455))
+            screen.blit(attack, (150, 430))
+            screen.blit(phy, (200, 455))
 
-    screen.blit(magic, (150, 500))
-    screen.blit(chol, (200, 528))
+            screen.blit(magic, (150, 500))
+            screen.blit(chol, (200, 528))
 
-    screen.blit(surrender, (500, 460))
-    screen.blit(stats, (500, 490))
+            screen.blit(surrender, (500, 460))
+            screen.blit(stats, (500, 490))
 
-    pygame.draw.line(screen, (255, 255, 255), (0, 400), (800, 400), 8)
+            pygame.draw.line(screen, (255, 255, 255), (0, 400), (800, 400), 8)
 
-    life = life - 0.05
+            coor[0] += 8
+        while coor[0] >= 240:
+            clock.tick(45)
 
-    player_health = utilities.r_text("{:.2f}".format(life), (220, 20, 60),
-                                     "./Assets/Fonts/bitwise.ttf", 30)
+            pygame.display.update()
+            pygame.display.flip()
 
-player_health = utilities.r_text("{:.2f}".format(life), (225, 225, 225),
-                                 "./Assets/Fonts/bitwise.ttf", 30)
+            screen.blit(back, (0, 0))
+            screen.blit(bat_bg, (0, 400))
+            screen.blit(avatar, (5, 420))
+            screen.blit(player_health, (7, 530))
 
+            screen.blit(enemy_, (coor[0], coor[1]))
 
-if life <= 0:
-    player_die = True
-elif monster.health <= 0:
-    monster_die = True
+            screen.blit(attack, (150, 430))
+            screen.blit(phy, (200, 455))
+
+            screen.blit(magic, (150, 500))
+            screen.blit(chol, (200, 528))
+
+            screen.blit(surrender, (500, 460))
+            screen.blit(stats, (500, 490))
+
+            pygame.draw.line(screen, (255, 255, 255), (0, 400), (800, 400), 8)
+
+            coor[0] -= 8
+
+        # Player dmg
+
+        coor = [280, 130]
+
+        while coor[1] > 100:
+            clock.tick(45)
+            pygame.display.update()
+            pygame.display.flip()
+
+            screen.blit(back, (0, 0))
+            screen.blit(bat_bg, (0, 400))
+            screen.blit(avatar, (5, 420))
+            screen.blit(player_health, (7, 530))
+
+            screen.blit(enemy_, (280, 130))
+
+            screen.blit(attack, (150, 430))
+            screen.blit(phy, (200, 455))
+
+            screen.blit(magic, (150, 500))
+            screen.blit(chol, (200, 528))
+
+            screen.blit(surrender, (500, 460))
+            screen.blit(stats, (500, 490))
+
+            pygame.draw.line(screen, (255, 255, 255), (0, 400), (800, 400), 8)
+
+            if player.health > 0:
+                screen.blit(a_player, (coor[0], coor[1]))
+
+            coor[1] -= 1
 
 is_anim = False
-
-sleep(0.3)
